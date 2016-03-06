5fa02c2d14e73b7e26ac7448c955daeed33ac588
Yfiua <yfiua@qq.com>
2016-03-06 16:59:18 +0000

    update script

--- kaggle-santander/main.py
+++ kaggle-santander/main.py
@@ -45,43 +45,25 @@ X_test = df_test.drop(['ID'], axis=1).values
 len_train = len(X_train)
 len_test  = len(X_test)
 
-# normal case
-X_train_norm = [ X_train[i] for i in range(len_train) if X_train[i, -1] != 117310.979016494 ]
-y_train_norm = [ y_train[i] for i in range(len_train) if X_train[i, -1] != 117310.979016494 ]
-X_test_norm  = [ X_test[i]  for i in range(len_test)  if X_test[i, -1]  != 117310.979016494 ]
-id_test_norm = [ id_test[i] for i in range(len_test)  if X_test[i, -1]  != 117310.979016494 ]
+# missing values
+for i in range(len_train):
+    if X_train[i, -1] == 117310.979016494:
+        X_train[i, -1] = np.nan
 
-X_train_norm = np.array(X_train_norm)
-
-# special case
-X_test_spec  = [ X_test[i]  for i in range(len_test)  if X_test[i, -1]  == 117310.979016494 ]
-id_test_spec = [ id_test[i] for i in range(len_test)  if X_test[i, -1]  == 117310.979016494 ]
-
-X_train_spec = np.array(X_train)[:,0:-1]
-y_train_spec = y_train
-X_test_spec  = np.array(X_test)[:,0:-1]
+for i in range(len_test):
+    if X_test[i, -1] == 117310.979016494:
+        X_test[i, -1] = np.nan
 
 # classifier
-clf_norm = xgb.XGBClassifier(max_depth=7, n_estimators=300, learning_rate=0.05, nthread=4)
-clf_spec = xgb.XGBClassifier(max_depth=7, n_estimators=300, learning_rate=0.05, nthread=4)
+clf = xgb.XGBClassifier(missing=np.nan, max_depth=8, n_estimators=400, learning_rate=0.05, nthread=4, subsample=0.9, colsample_bytree=0.85)
 
-#scores = cross_validation.cross_val_score(clf_norm, X_train_norm, y_train_norm, scoring='roc_auc', cv=5)
-#print(scores.mean())
-
-X_train_norm, X_eval_norm, y_train_norm, y_eval_norm = train_test_split(X_train_norm, y_train_norm, test_size=0.33, random_state=42)
-X_train_spec, X_eval_spec, y_train_spec, y_eval_spec = train_test_split(X_train_spec, y_train_spec, test_size=0.33, random_state=42)
+X_train, X_eval, y_train, y_eval= train_test_split(X_train, y_train, test_size=0.33, random_state=142)
 
 # fitting
-clf_norm.fit(X_train_norm, y_train_norm, early_stopping_rounds=10, eval_metric="auc", eval_set=[(X_eval_norm, y_eval_norm)])
-clf_spec.fit(X_train_spec, y_train_spec, early_stopping_rounds=10, eval_metric="auc", eval_set=[(X_eval_norm, y_eval_norm)])
+clf.fit(X_train, y_train, early_stopping_rounds=20, eval_metric="auc", eval_set=[(X_eval, y_eval)])
 
 # predicting
-y_pred_norm = clf_norm.predict_proba(X_test)
-y_pred_spec = clf_spec.predict_proba(X_test_spec)
-
-# ensemble
-y_pred = [ y_pred_norm[i] if X_test[i, -1] != 117310.979016494 else y_pred_spec[i] for i in range(len_test) ]
-y_pred = np.array(y_pred)
+y_pred= clf.predict_proba(X_test)
 
 submission = pd.DataFrame({"ID":id_test, "TARGET":y_pred[:,1]})
 submission.to_csv("submission.csv", index=False)
